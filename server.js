0811095185b928329214368e50da3124f5fb92ff
vishkharat <154818939+vishkharat@users.noreply.github.com>
2024-04-01 12:58:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ const options = {
     },
     servers: [
       {
-              url: "http://localhost:8080",
-           // url: "https://nodejs-job-portal-app.onrender.com"
+              // url: "http://localhost:8080",
+            url: "https://careerconnect-nodejs-jobportal.onrender.com"
       },
     ],
   },
