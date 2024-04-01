7ccdc2df7e99d8c3f1104f0b9be025272cf3731e
vishkharat <154818939+vishkharat@users.noreply.github.com>
2024-04-01 13:17:01 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,10 +4,10 @@ import colors from "colors";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
-    // console.log(
-    //   `Connected To Mongodb Database ${mongoose.connection.host}`.bgMagenta
-    //     .white
-    // );
+    console.log(
+      `Connected To Mongodb Database ${mongoose.connection.host}`.bgMagenta
+        .white
+    );
   } catch (error) {
     // console.log(`MongoDB Erorr ${error}`.bgRed.white);
   }
